be57939cc1db847db0c51865e0edd7b58df16967
Schutz, Erich <Erich.Schutz@aesynt.com>
2015-10-01 13:09:01 -0400

    update after review

--- js/app.js
+++ js/app.js
@@ -51,13 +51,15 @@ Enemy.prototype.render = function() {
  * @param width - The width of the player sprite
  * @param speed - The speed of the player in the game
  */
-var Player = function(x, y, height, width, speed){
+var Player = function(x, y, height, width, speed, score, level){
     this.sprite = 'images/char-boy.png';
     this.x = x;
     this.y = y;
     this.height = height;
     this.width = width;
     this.speed = speed;
+    this.score = score;
+    this.level = level;
 };
 
 /**
@@ -73,10 +75,54 @@ Player.prototype.reset = function(){
  */
 Player.prototype.update = function(){
     
-    checkPlayerBounds(this);
-
-    checkIfPlayerMadeIt(this);
+    if (this.y > 400 ) {
+        this.y = 400;
+    }
+    if (this.x > 418) {
+        this.x = 418;
+    }
+    if (this.x < -15) {
+        this.x = -15;
+    }
+    
+    if(this.y < -20){
+        this.reset();
+        this.level += 1;
+        this.determineScore('madeIt');
+        
+        //levelUp(this.score, this.level);
+        this.levelUp();
+    }
+    
 };
+
+Player.prototype.determineScore = function(scoreType){
+  if(this.score <= 0){
+       this.score = 0;
+   }
+
+   if(scoreType === 'madeIt'){
+       this.score += 10;
+   }
+
+   if(scoreType === 'collided'){
+       this.score -= 1;
+   }
+};
+
+Player.prototype.levelUp = function(){
+    
+  allEnemies.splice(0, allEnemies.length);
+      
+    for (var i = 0; i <= this.gameLevel - 1; i++) {
+        var enemy = new Enemy(0, Math.random() * 184 + 50, 135, 88, Math.random() * 256);
+        allEnemies.push(enemy);
+    }
+      
+    displayScore(this.score);
+    displayLevel(this.level);
+};
+
 
 /**
  * @description render - Draws the player on the canvas
@@ -91,16 +137,16 @@ Player.prototype.render = function () {
  */
 Player.prototype.handleInput = function(keyPress) {
     if (keyPress == 'left') {
-        player.x -= player.speed;
+        this.x -= this.speed;
     }
     if (keyPress == 'up') {
-        player.y -= player.speed - 5;
+        this.y -= this.speed - 5;
     }
     if (keyPress == 'right') {
-        player.x += player.speed;
+        this.x += this.speed;
     }
     if (keyPress == 'down') {
-        player.y += player.speed - 5;
+        this.y += this.speed - 5;
     }
  };
 
@@ -116,24 +162,14 @@ Player.prototype.handleInput = function(keyPress) {
          player.x + player.width > enemy.x + 11) {
          
          player.reset();
-         playerScore =  determinePlayerScore(playerScore, 'collided');
-         displayScore(playerScore);
+         //playerScore =  determinePlayerScore(playerScore, 'collided');
+
+         player.determineScore('collided');
+         
+         displayScore(player.score);
      }
 
  };
-
-/**
- * @description checkIfPlayerMadeIt - Checks if the player made it to the end goal.
- * @param player {object} - This represents the player in the game.
- */
-var checkIfPlayerMadeIt = function(player){
-    if(player.y < -20){
-        player.reset();
-        gameLevel += 1;
-        playerScore = determinePlayerScore(playerScore, 'madeIt');
-        levelUp(playerScore, gameLevel);
-    }
-};
 
 /**
  * @description getCanvas - Returns the canvas tag on the DOM to append the level and score to.
@@ -149,9 +185,8 @@ var getCanvas = function(){
  * @param playerScore {int} - The current player score
  */
 var displayScore = function(playerScore){
-   var canvasElement =  getCanvas();
-    playerScore = determinePlayerScore(playerScore);
-    scoreDisplay.innerHTML = 'Score: ' + playerScore;
+   var canvasElement =  getCanvas();    
+    scoreDisplay.innerHTML = 'Score: ' + player.score;
     document.body.insertBefore(scoreDisplay, canvasElement);
 };
 
@@ -160,32 +195,10 @@ var displayScore = function(playerScore){
  * @param gameLevel
  */
 var displayLevel = function(gameLevel){
+    
     var firstCsElement = getCanvas(); 
-    levelDisplay.innerHTML = 'Level: ' + gameLevel+' ';
+    levelDisplay.innerHTML = 'Level: ' + player.level +' ';
     document.body.insertBefore(levelDisplay, firstCsElement);
-};
-
-/**
- * @description determinePlayerScore - Determines the players current score based on scoring type. If no scoring type is passed in, the score will be returned. The score can not go below 0.
- * @param score {int} - The current score it the game.
- * @param scoreType {string} - The scope type, either madeIt or collided
- * @returns the current score.
- */
-var determinePlayerScore = function(score, scoreType){
-
-   if(score <= 0){
-       score = 0;
-   }
-
-   if(scoreType === 'madeIt'){
-       score += 10;
-   }
-
-   if(scoreType === 'collided'){
-       score -= 1;
-   }
-
-   return score;
 };
 
 /**
@@ -207,36 +220,18 @@ var levelUp = function(playerScore, gameLevel){
   };
 
 /**
- * @description checkPlayerBounds - Checks to make sure the player will not navigate off of the canvas.
- * @param player {object} - This represents the player in the game.
- */
-var checkPlayerBounds = function(player) {
-    if (player.y > 400 ) {
-        player.y = 400;
-    }
-    if (player.x > 418) {
-        player.x = 418;
-    }
-    if (player.x < -15) {
-        player.x = -15;
-    }
-};
-
-/**
  * @description Initialization of the game. The game starts with one enemy and one player.
  */
 var allEnemies = [];
 allEnemies.push(new Enemy(0, Math.random() * 184 + 50, 135, 88, Math.random() * 256 ));
 
-var player = new Player(202.5, 383, 131, 76, 50);
-var playerScore = 0;
-var gameLevel = 1;
+var player = new Player(202.5, 383, 131, 76, 50, 0, 1);
 
 var scoreDisplay = document.createElement('div');
 var levelDisplay = document.createElement('div');
 
-displayScore(playerScore);
-displayLevel(gameLevel);
+displayScore(0);
+displayLevel(1);
 
 /**
  * @description event listener for the keyup DOM event.
